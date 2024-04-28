77a9592bcaa55f3f698804dadbafc708c9d821d2
pranshubadhani <pranshubadhani@gmail.com>
2024-04-28 20:27:50 +0530

    First Commit

---
+++ frontend/src/Components/services/PlumbingRepairs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import Footer from "./Footer";
+// import Footer from "./contact-page/Footer";
+
+function PlumbingRepairs(){
+    return(
+        <>
+        {/* <Footer/> */}
+        <h1>Plumbing Repairs</h1>
+        </>
+    )
+}
+export default PlumbingRepairs;
